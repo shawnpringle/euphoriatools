bfb9185cee3d5803d7296390ed1a03f3cd2ee4b9
Shawn Pringle <shawn.pringle@gmail.com>
2020-09-21 19:02:29 -0300

    * allow for sequences of string literals for open_dll.

--- source_check.ex
+++ source_check.ex
@@ -26,6 +26,17 @@ include std/text.e
 include std/io.e
 include std/search.e
 include std/os.e
+include std/get.e
+include std/pretty.e
+
+type sequence_of_cstring(sequence s)
+    for i = 1 to length(s) do
+        if not cstring(s[i]) then
+            return 0
+         end if
+     end for
+     return 1
+end type
 
 cstring euphoria_exe, source_check_ex, dll_filename = "", e_filename = ""
 procedure usage()
@@ -107,12 +118,15 @@ sequence et_tokens
 integer et_error, et_err_line, et_err_column
 integer succeses = 0, attempts = 0
 keep_whitespace(0)
+return_literal_string(1)
+string_strip_quotes(0)
 {et_tokens, et_error, et_err_line, et_err_column} = tokenize_file(e_filename)
 if et_error != ERR_NONE then
     display("Error loading E file")
     maybe_any_key()
     abort(1)
 end if
+
 for tok_i = 1 to length(et_tokens)-2 do
     sequence tok = et_tokens[tok_i]
     if tok[TTYPE] = T_IDENTIFIER then
@@ -139,14 +153,16 @@ for tok_i = 1 to length(et_tokens)-2 do
 	                 {dll_filename, dll_id} = r        	
                 end if
                 tok = et_tokens[tok_i+4]
-                atom function_load = define_c_proc(dll_id, tok[TDATA], {})
+                cstring function_name = tok[TDATA][2..$-1]
+                atom function_load = define_c_proc(dll_id, function_name, {})
                 attempts += 1
                 if function_load < 0 then
-                    display("Could not load '[2]' from '[1]' in '[3]' line [4], column [5].", { dll_filename, tok[TDATA], e_filename, tok[TLNUM], tok[TLPOS] })
+                    display("Could not load [2] from [1] in '[3]' line [4], column [5].", { dll_filename, tok[TDATA], e_filename, tok[TLNUM], tok[TLPOS] })
                 else
                     succeses += 1
                 end if
             case "open_dll" then
+                cstring this_dll_filename = ""
                 if tok_i - 2 < 1 or tok_i + 2 > length(et_tokens) then
                     display("Irrational code at the beginning of the file or syntax error at the end of the file")
                     continue
@@ -154,7 +170,7 @@ for tok_i = 1 to length(et_tokens)-2 do
                 sequence equals_tok = et_tokens[tok_i-1]
                 sequence before_equals_tok = et_tokens[tok_i-2]
                 
-                if (equals_tok[TTYPE] != T_EQ) or (before_equals_tok[TTYPE] != T_IDENTIFIER) or et_tokens[tok_i+1][TTYPE] != T_LPAREN or et_tokens[tok_i+2][TTYPE] != T_STRING then
+                if (equals_tok[TTYPE] != T_EQ) or (before_equals_tok[TTYPE] != T_IDENTIFIER) or et_tokens[tok_i+1][TTYPE] != T_LPAREN  then
                     cstring e
 
                     if equals_tok[TTYPE] != T_EQ then
@@ -166,13 +182,53 @@ for tok_i = 1 to length(et_tokens)-2 do
                     	e = "Left parenthesis did not follow open_dll."
                     elsif equal(et_tokens[tok_i+2][TDATA], "{") then
                         e = "open_dll(sequence of strings) is not supported by source_check.ex"
-                    else
-                    	e = "First argument to open_dll isn't a string.  It's " & et_tokens[tok_i + 2][TDATA] & " in" 
- 		    end if
-                    display(e & " [1] L: [2] C: [3]", { e_filename, tok[TLNUM], tok[TLPOS] })
-                    continue
-                end if
-                cstring this_dll_filename = et_tokens[tok_i+2][TDATA]
+                    end if
+		    display(e & " [1] L: [2] C: [3]", { e_filename, tok[TLNUM], tok[TLPOS] })
+		    continue
+		end if
+                sequence vbuf = {GET_FAIL, 0}
+		if et_tokens[tok_i+2][TTYPE] != T_STRING then
+                        cstring strbuf = ""
+                    	for tok_j = tok_i+2 to length(et_tokens) do
+                    		if not cstring(et_tokens[tok_j][TDATA]) then
+                    			display("Data is not a string: [1]  [2] L: [3] C:[4]", {et_tokens[tok_j][TDATA], e_filename, et_tokens[tok_j][TLNUM], et_tokens[tok_j][TLPOS]})
+                    			break
+                    		end if
+                    		strbuf &= et_tokens[tok_j][TDATA]
+                    		vbuf = value(strbuf)
+                    		if vbuf[1] = GET_SUCCESS then
+                    			exit
+                    		end if
+                    	end for
+                    	if length(strbuf) > 60 then
+                    		strbuf = strbuf[1..60] & "..."
+                    	end if
+                    	if vbuf[1] = GET_SUCCESS then
+                    		-- so it's a EUPHORIA literal of some kind.
+                    		-- Let's suppose it is a sequence of strings
+                    		if sequence_of_cstring(vbuf[2]) then
+                    			for vi = 1 to length(vbuf[2]) do
+                    				cstring s = vbuf[2][vi]
+                    				if open_dll(s) != 0 then
+                    					this_dll_filename = s
+                    				end if
+                    			end for
+                    			if equal(this_dll_filename,"") then
+                    				display("None of these dlls could be opened: [1]", {strbuf})
+                    				this_dll_filename = strbuf
+                    			end if
+                    		else
+					 display("First argument to open_dll isn't a sequence of cstirngs literal .  It's [4]. \n  [1] L: [2] C: [3]", { strbuf, e_filename, tok[TLNUM], tok[TLPOS] })
+					 continue
+                    		end if                      
+                    	else
+                    	         display("First argument to open_dll isn't a EUPHORIA literal of any kind.  It's [4]. \n  [1] L: [2] C: [3]", { strbuf, e_filename, tok[TLNUM], tok[TLPOS] })
+                    	         continue
+                    	end if -- GET_SUCCESS
+                end if -- x = open_dll(NOT_A_STRING_LITERAL)
+                if equal(this_dll_filename, "") and length(et_tokens[tok_i+2][TDATA]) > 1 then
+	                this_dll_filename = et_tokens[tok_i+2][TDATA][2..$-1]
+	        end if
                 cstring this_dll_idname   = before_equals_tok[TDATA]
                 integer add_flag = 1
                 integer dll_index = map_find(dlls, this_dll_idname)
@@ -181,7 +237,7 @@ for tok_i = 1 to length(et_tokens)-2 do
                 	add_flag = ( ends(this_dll_filename, ".dll") and (platform() = WINDOWS) ) or ( ends(this_dll_filename, ".dylib") and (platform() =OSX) )
                 		or ( ends(this_dll_filename, ".so") and not find(platform(), {WINDOWS, OSX}) )
                 end if
-                if add_flag then
+                if dll_index = 0 or add_flag then
                 	-- will clobber existing dll name and value if already there.
 	                dlls = map_put(dlls, this_dll_idname, {this_dll_filename, open_dll({pathname(e_filename) & SLASH & this_dll_filename, this_dll_filename})})
 	        end if
